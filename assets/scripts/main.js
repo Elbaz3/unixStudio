83e72762b399ac3e912c66aeef5dda119ed17a1c
Ahmed Elsayed Elbaz <ahmedelsayede75@gmail.com>
2025-10-31 13:28:53 +0200

    feat: add interactivity ( toggle menu, scroll top, hover effect on arrows and buttons )

---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+const list = document.querySelector(".nav__wrapper__list");
+const menuIcon = document.querySelector(".header__menu");
+const scrollUp = document.querySelector(".scroll-top");
+
+console.log(scrollUp);
+
+menuIcon.addEventListener("click", () => {
+  list.classList.toggle("toggle-open");
+  menuIcon.classList.toggle("rotate-icon");
+});
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
